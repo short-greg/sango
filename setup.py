9eaa6cb9a84185177292c2077583de6167b66726
Greg Short <shortg29@gmail.com>
2022-01-03 08:17:52 +0900

    add setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sango',
+    version='0.1.0',
+    packages=['sango']
+)
